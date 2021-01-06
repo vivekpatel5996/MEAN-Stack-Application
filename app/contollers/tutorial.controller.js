4dc5393075d90735315002ada5378e31d5f46e17
Vivek Patel <vivekjasubhai4@gmail.com>
2021-01-06 17:40:38 +0530

    Added editing tutorial functionality

--- app/contollers/tutorial.controller.js
+++ app/contollers/tutorial.controller.js
@@ -47,15 +47,65 @@ exports.findAll = (req, res) => {
 }
 
 exports.findOne = (req, res) => {
+    const id = req.params.id;
 
+    Tutorial.findById(id)
+        .then(data => {
+            if (!data)
+                res.status(404).send({ message: "Not found Tutorial with id " + id });
+            else res.send(data);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .send({ message: "Error retrieving Tutorial with id=" + id });
+        });
 };
 
 exports.update = (req, res) => {
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Data to update can not be empty!"
+        });
+    }
 
+    const id = req.params.id;
+
+    Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+                });
+            } else res.send({ message: "Tutorial was updated successfully." });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Tutorial with id=" + id
+            });
+        });
 };
 
 exports.delete = (req, res) => {
+    const id = req.params.id;
 
+    Tutorial.findByIdAndRemove(id)
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+                });
+            } else {
+                res.send({
+                    message: "Tutorial was deleted successfully!"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Tutorial with id=" + id
+            });
+        });
 };
 
 exports.deleteAll = (req, res) => {
--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -2,8 +2,25 @@ module.exports = app => {
     const tutorials = require("../contollers/tutorial.controller");
     var router = require("express").Router();
 
+    // Create a new Tutorial
     router.post("/", tutorials.create);
-    router.get("/",tutorials.findAll);
+
+    // Retrieve all Tutorials
+    router.get("/", tutorials.findAll);
+    // Retrieve all published Tutorials
+    router.get("/published", tutorials.findAllPublished);
+
+    // Retrieve a single Tutorial with id
+    router.get("/:id", tutorials.findOne);
+
+    // Update a Tutorial with id
+    router.put("/:id", tutorials.update);
+
+    // Delete a Tutorial with id
+    router.delete("/:id", tutorials.delete);
+
+    // Create a new Tutorial
+    router.delete("/", tutorials.deleteAll);
 
 
     app.use('/api/tutorials', router);
